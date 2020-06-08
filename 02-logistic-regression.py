c9bade5c17497a03224f514cb168df1887ab17b1
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-06-08 12:15:17 -0300

    added comments

--- 02-logistic-regression.py
+++ 02-logistic-regression.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 import seaborn as sns
 
+#Import the data set
 titanic_data = pd.read_csv('titanic_train.csv')
 
-# sns.heatmap(titanic_data.isnull(), cbar=False)
-# sns.countplot(x='Survived', data=titanic_data)
-# sns.countplot(x='Survived', hue='Sex', data=titanic_data)
-# sns.countplot(x='Survived', hue='Pclass', data=titanic_data)
-# plt.hist(titanic_data['Age'].dropna())
-# plt.hist(titanic_data['Fare'])
+#Exploratory data analysis
+sns.heatmap(titanic_data.isnull(), cbar=False)
+sns.countplot(x='Survived', data=titanic_data)
+sns.countplot(x='Survived', hue='Sex', data=titanic_data)
+sns.countplot(x='Survived', hue='Pclass', data=titanic_data)
+plt.hist(titanic_data['Age'].dropna())
+plt.hist(titanic_data['Fare'])
 sns.boxplot(titanic_data['Pclass'], titanic_data['Age'])
 
+#Imputation function
 def impute_missing_age(columns):
     age = columns[0]
     passenger_class = columns[1]
@@ -28,39 +31,48 @@ def impute_missing_age(columns):
         
     else:
         return age
-    
+
+#Impute the missing Age data
 titanic_data['Age'] = titanic_data[['Age', 'Pclass']].apply(impute_missing_age, axis = 1)
 
-# sns.heatmap(titanic_data.isnull(), cbar=False)
+#Reinvestigate missing data
+sns.heatmap(titanic_data.isnull(), cbar=False)
+
+#Drop null data
 titanic_data.drop('Cabin', axis=1, inplace = True)
 titanic_data.dropna(inplace = True)
 
+#Create dummy variables for Sex and Embarked columns
 sex_data = pd.get_dummies(titanic_data['Sex'], drop_first = True)
 embarked_data = pd.get_dummies(titanic_data['Embarked'], drop_first = True)
 
+#Add dummy variables to the DataFrame and drop non-numeric data
 titanic_data = pd.concat([titanic_data, sex_data, embarked_data], axis = 1)
-
 titanic_data.drop(['Name', 'PassengerId', 'Ticket', 'Sex', 'Embarked'], axis = 1, inplace = True)
 
+#Print the finalized data set
 titanic_data.head()
 
+#Split the data set into x and y data
 y_data = titanic_data['Survived']
 x_data = titanic_data.drop('Survived', axis = 1)
 
+#Split the data set into training data and test data
 from sklearn.model_selection import train_test_split
 x_training_data, x_test_data, y_training_data, y_test_data = train_test_split(x_data, y_data, test_size = 0.3)
 
+#Create the model
 from sklearn.linear_model import LogisticRegression
-
 model = LogisticRegression()
 
+#Train the model and create predictions
 model.fit(x_training_data, y_training_data)
-
 predictions = model.predict(x_test_data)
 
+#Calculate performance metrics
 from sklearn.metrics import classification_report
-
 print(classification_report(y_test_data, predictions))
 
+#Generate a confusion matrix
 from sklearn.metrics import confusion_matrix
 print(confusion_matrix(y_test_data, predictions))
