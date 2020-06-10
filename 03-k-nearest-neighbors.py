12efd361c6a71bdf4ba2d57cafd4a5de16ebe948
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-06-10 11:42:24 -0300

    Update 03-k-nearest-neighbors.py

--- 03-k-nearest-neighbors.py
+++ 03-k-nearest-neighbors.py
@@ -1,1 +1,39 @@
+import numpy as np
+import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+raw_data = pd.read_csv('classified_data.csv', index_col = 0)
+print(raw_data.columns)
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+scaler.fit(raw_data.drop('TARGET CLASS', axis=1))
+
+scaled_features = scaler.transform(raw_data.drop('TARGET CLASS', axis=1))
+
+scaled_data = pd.DataFrame(scaled_features, columns = raw_data.drop('TARGET CLASS', axis=1).columns)
+
+from sklearn.model_selection import train_test_split
+
+x = scaled_data
+y = raw_data['TARGET CLASS']
+
+x_training_data, x_test_data, y_training_data, y_test_data = train_test_split(x, y, test_size = 0.3)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors = 1)
+
+model.fit(x_training_data, y_training_data)
+predictions = model.predict(x_test_data)
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+print(classification_report(y_test_data, predictions))
+print(confusion_matrix(y_test_data, predictions))
